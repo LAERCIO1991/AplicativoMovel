c13ba3d5b7466a2624018288f515132f6e164c88
LAERCIO1991 <laercio.msj@hotmail.com>
2013-11-07 11:27:02 -0200

    primeiro

--- conversorClasse.js
+++ conversorClasse.js
@@ -15,6 +15,35 @@
 
 
 			
+			    // Wait for device API libraries to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+    }
+
+    // onSuccess: Get a snapshot of the current acceleration
+    //
+    function onSuccess(acceleration) {
+        alert('Acceleration X: ' + acceleration.x + '\n' +
+              'Acceleration Y: ' + acceleration.y + '\n' +
+              'Acceleration Z: ' + acceleration.z + '\n' +
+              'Timestamp: '      + acceleration.timestamp + '\n');
+    }
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+        alert('onError!');
+    }
+			
+			
+			
+			
+			
 			function checkConnection() {
 				var networkState = navigator.connection.type;
 				var states = {};
